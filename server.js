affa682948266079a45e0cf13b31f417146abac1
fazliddin-web79 <yakubjonovfazliddin777@gmail.com>
2023-04-22 02:20:22 +0500

    all database cloud

--- server.js
+++ server.js
@@ -64,17 +64,44 @@ app.post("/webhook", async (req, res) => {
   res.json({ received: true });
 });
 
-mongoose
-  .connect("mongodb://localhost:27017/online-market", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log("connected to MongoDB");
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+mongoose.connect(
+  "mongodb+srv://yakubjonovfazliddin777:NgnKjv2mcReaIV9C@cluster0.hlkpdtd.mongodb.net/online-market?retryWrites=true&w=majority",
+  () => {
+    console.log("Database connected");
+  }
+);
+// const url =
+//   "mongodb+srv://yakubjonovfazliddin777:NgnKjv2mcReaIV9C@cluster0.hlkpdtd.mongodb.net/online-market?retryWrites=true&w=majority";
+
+// const dbName = "online-market";
+
+// mongoose
+//   .connect(url, { useNewUrlParser: true })
+//   .catch((err) => console.log(err));
+
+// const db = mongoose.connection;
+
+// db.on("error", console.error.bind(console, "MongoDB connection error:"));
+
+// db.once("open", function () {
+//   console.log("Connected successfully to server");
+
+//   // Perform database operations here
+
+//   db.close();
+// });
+
+// mongoose
+//   .connect("mongodb://localhost:27017/online-market", {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() => {
+//     console.log("connected to MongoDB");
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   });
 
 app.listen(port, () => {
   console.log(`server is running on port ${port}`);
